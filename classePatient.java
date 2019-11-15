1e2444290ca8bacf24889852196627e2889747b3
Streignard-M <56094411+StreignardM@users.noreply.github.com>
2019-11-15 10:27:04 +0100

    Update classePatient.java

--- classePatient.java
+++ classePatient.java
@@ -20,32 +20,15 @@ public class Patient {
                '}';
    }
 
-   public void setNom(String nom) {
-       this.nom = nom;
-   }
-   public void setPrenom(String prenom) {
-       this.prenom = prenom;
-   }
-   public void setNaissance(Date naissance) {
-       this.naissance = naissance;
-   }
-   
-   public void setNumeroNational(String numeroNational) {
-       this.numeroNational = numeroNational;
-   }
-   public void setEntree(Date entree) {
-       this.entree = entree;
-   }
 
-
-  void sortie(){
+  static void sortie(){
        // Le patient sort et son objet est supprimé !
        /**
         * Un appel vers la base de données pour retirer le patient des patients en attente ou listés.
         * Via a Webservice
         */
    }
-   void changementService(){
+   static void changementService(){
        /** Le patient est déplacé vers la chambre d'un autre service.
         * Le patient est retiré de la chambre et remis dans une autre appartenant à un autre service.
         * Via Webservice
@@ -69,10 +52,27 @@ public class Patient {
        this.setNaissance(naissance);
        this.setNumeroNational(numeroNational);
    }
+   
+   public void setNom(String nom) {
+       this.nom = nom;
+   }
+   public void setPrenom(String prenom) {
+       this.prenom = prenom;
+   }
+   public void setNaissance(Date naissance) {
+       this.naissance = naissance;
+   }
+   
+   public void setNumeroNational(String numeroNational) {
+       this.numeroNational = numeroNational;
+   }
+   public void setEntree(Date entree) {
+       this.entree = entree;
+   }
 
    public static void main(String [] args) {
        Patient numberOne = new Patient();
        numberOne.creerPatient("Duponteuil", "Jean-Michel", new Date(1986, 9, 26));
-       numberOne.toString();
+       System.out.println(numberOne.toString();
    }
 }
